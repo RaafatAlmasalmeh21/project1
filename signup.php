1e3c09d63aaeb4e98b682f30467d36c2b9f0a106
Raafat_ALMASALMEH <2115051@talnet.nl>
2020-09-09 12:39:44 +0200

    dit zijn niuwe files

---
+++ signup.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+ 
+<form action="signup.php">
+
+
+<input type="text" name= "firstname" placeholder="firstname" required>
+<input type="text" name= "insertion" placeholder="Insertion" >
+<input type="text" name= "lastname" placeholder="lastname" required>
+<input type="email" name= "email" placeholder="email" required>
+<input type="text" name= "username" placeholder="username" required>
+<input type="password" name= "password" placeholder="password" required>
+<input type="password" name= "repeat_password" placeholder="repeat password" required>
+<button type= "submit" >register</button>
+
+</form>
+
+
+
+</body>
+
+</html>
